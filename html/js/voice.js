5a9a332025eb3513dd781d835b120fda4af24728
Boaz Ziniman <boazz@amazon.com>
2017-12-07 16:45:04 +0200

    Adding basic voice recording in web page

---
+++ html/js/voice.js
@@ -0,0 +1,34 @@
+var onFail = function(e) {
+  console.log('Rejected!', e);
+};
+
+var onSuccess = function(s) {
+  var context = new AudioContext();
+  var mediaStreamSource = context.createMediaStreamSource(s);
+  recorder = new Recorder(mediaStreamSource);
+  recorder.record();
+
+  // audio loopback
+  // mediaStreamSource.connect(context.destination);
+}
+
+window.URL = window.URL || window.webkitURL;
+navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+var recorder;
+var audio = document.querySelector('audio');
+
+function startRecording() {
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({audio: true}, onSuccess, onFail);
+  } else {
+    console.log('navigator.getUserMedia not present');
+  }
+}
+
+function stopRecording() {
+  recorder.stop();
+  recorder.exportWAV(function(s) {
+    audio.src = window.URL.createObjectURL(s);
+  });
+}
